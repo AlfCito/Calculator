fe19b5c4371f8f5bbe56da96d77aedc2d5dd8dd8
Eddie Yepez <eddieyepez@yahoo.com>
2018-05-11 21:56:07 -0400

    Working app
    Need to user test for bugs

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 window.onload = function() {
 
+  let preMath = 0;
+  let result = 0;
+  let operation = '';
+  let chain = false;
+  let firstNoneZero = false;
+
+  let alertText = "Digit Limit Met";
+
   let buttons = document.getElementsByTagName("button");
   let screen = document.getElementById("screen");
   let subscreen = document.getElementById("subscreen");
@@ -10,250 +18,243 @@ window.onload = function() {
     });
   }
 
-  let data = {  
-    currentNumber : '',  
-    previousNumber : '',  
-    substring : [],
-    result : 0,
-    finished : false,
-    isZero : true,
-  }
+  let getData = (value) => {
 
-  let checkZero = () => {
-    //if(screen.innerHTML === '0'){
-    if(
-      data.previousNumber === data.result 
-      || data.previousNumber === '0' 
-      //|| screen.innerHTML === '0' 
-      //|| data.currentNumber === ''
-    ){
-      data.isZero = true;
-      /*if(data.substring.length > 1){
-        data.isZero = false;
-      }*/
+    if(value === '/' || value === 'x' || value === '+' || value === '-'){
+      addOperator(value);
+    }else if(value === 'CE'){
+      clearLast();
+    }else if(value === 'C'){
+      clear();
+    }else if(value === 's'){
+      signChange();
+    }else if(value === '='){
+      calculate(preMath);
     }else{
-      data.isZero = false;
+      createNumber(value);      
+    }
+
+  }
+
+  //create numbers
+  let createNumber = (value) => {
+
+    //if number dont excede the available space
+    if(screen.innerHTML.length < 15){
+
+      // if the display is in 0
+      if(screen.innerHTML === '0'){
+
+        if(subscreen.innerHTML === alertText){
+          subscreen.innerHTML = '';
+        }
+
+        //if input is going to be with decimal point
+        if(value === '.'){
+          // keep the 0 and .
+          screen.innerHTML = '0' + value;
+        }else{ //if is not decimal
+          //replace the 0, this becomes the new first no zero
+          screen.innerHTML = value;
+        }
+
+      }else{  //if the display is not in 0
+
+        if(subscreen.innerHTML === alertText){
+          subscreen.innerHTML = '';
+        }
+
+        //is value ia a new first number 
+        if(firstNoneZero === true){ 
+          //replace the current number
+          screen.innerHTML = value;
+          // after replacing the first number remove the state to keep adding characters
+          chain = false;
+          firstNoneZero = false;
+        }else{  //if is not the first number
+
+          if(value === '.'){
+
+            let counter = 0;
+            for(let i = 0; i < screen.innerHTML; i++){
+              if( screen.innerHTML[i] === '.'){
+                counter++;
+              }
+            }
+
+            if(counter < 1){
+              screen.innerHTML += value;
+            }
+
+          }else{
+            //keep adding number characters
+            screen.innerHTML += value;
+          }       
+          
+        }      
+
+      }
+
+    }
+    
+  }
+
+  // add math operator
+  let addOperator = (value) => {
+
+    // => user has NOT input any number => user is just presing the operations btns
+    if(chain === true){      
+
+      if(subscreen.innerHTML === ''){
+        subscreen.innerHTML += screen.innerHTML + ' ' + value + ' '; 
+        //prepare for a chained operation (adding more numbers and signs)
+        chain = true; 
+        //set ready for new number
+        firstNoneZero = true;
+        // set the value of the operation to use in preCalculus
+        if(operation === ''){
+          //if the value of operation was reseted by the = operation; set it to the new value
+          operation = value;
+        }      
+        //calculate and temporary store the results of the subscreen operations     
+        preCalculus();
+      }else if(subscreen.innerHTML === alertText){
+          subscreen.innerHTML = '';
+      }else{
+        //remove the last 2 characters 
+        subscreen.innerHTML = subscreen.innerHTML.slice(0, -2) + value + ' ';
+      }
+          
+    }else{  // chain === false => user has input some number
+      // add the number and the sign of the operation to the subscreen
+      subscreen.innerHTML += screen.innerHTML + ' ' + value + ' ';  
+      //prepare for a chained operation (adding more numbers and signs)
+      chain = true; 
+      //set ready for new number
+      firstNoneZero = true;
+      // set the value of the operation to use in preCalculus
+      if(operation === ''){
+        //if the value of operation was reseted by the = operation; set it to the new value
+        operation = value;
+      }      
+      //calculate and temporary store the results of the subscreen operations     
+      preCalculus();
+    }
+    // set the value of the current operation
+    operation = value;    
+
+  }
+
+  //Calculate value of substring elements
+  let preCalculus = () => {
+    //get the current display number
+    let displayNum = Number(screen.innerHTML);
+    // if user haven't input any number
+    if(preMath === 0){
+      //leave the 0 in the display
+      preMath = displayNum;
+    }else if(preMath !== 0){ //if the user enter some input
+      // calculate and store 
+      preMath = doMath(preMath, displayNum);
+    }
+
+    //set the amount of decimal points
+    let pres = 14 - preMath.toString().indexOf('.');  
+    // round the result and display it on the screen  
+    screen.innerHTML = round(preMath, pres);
+  }
+
+  //Calculate the final result
+  let calculate = (numMemory) =>{
+    //get the current display number
+    let display = Number(screen.innerHTML);
+    // calculate the result
+    result = doMath(numMemory, display);    
+
+    //set the amount of decimal points
+    let pres = 14 - result.toString().indexOf('.'); 
+    // round the result and display it on the screen 
+    let roundResult = round(result, pres).toString();  
+/*
+    screen.innerHTML = roundResult;
+    //since is the final result erase the subscreen display
+    subscreen.innerHTML = '';
+*/
+
+    if(roundResult.length <= 14){
+      screen.innerHTML = roundResult;
+      //since is the final result erase the subscreen display
+      subscreen.innerHTML = '';
+    }else{
+
+      if(roundResult.indexOf('e') !== -1){
+        clear();
+        subscreen.innerHTML = alertText;
+      }else{
+        screen.innerHTML = roundResult.slice(0, 14);
+        subscreen.innerHTML = '';
+      }
+    }
+    
+
+    
+    //and reset the variables
+    preMath = 0;
+    result = 0;
+    operation = '';
+    // be prepare for chained values
+    chain = true;
+    // be prepare for next number to be the first none zero
+    firstNoneZero = true;
+  }
+
+  //do mathematical operations
+  let doMath = (numA, displayNum) => { 
+
+    if(operation === '/'){
+      return numA / displayNum;
+    }
+    if(operation === 'x'){
+      return numA * displayNum;
+    }
+    if(operation === '+'){
+      return numA + displayNum;
+    }
+    if(operation === '-'){
+      return numA - displayNum;
     }
   }
 
-  let getData = (value) => {   
-    if( value === 'x' || value === '/' || value === '+' || value === '-'|| value === '='){ // if is a mathematical operator
-      operation(value);
-    }else if( value === 'CE'){ // else if is CE
-      resetLast();
-    }else if(value === 'C'){ // else if is C
-      resetAll();
-    }else if ( value === 's'){ // else if is s for sign change
-      switchSign();
-    }else{  // else if user input is a number or point
-      createNumber(value);
-    }
-  }
+  let signChange = () => {
 
-  let switchSign = () => {
-
-    if( screen.innerHTML !== '0' ){
-
-      let temp;
+    let temp;
+    if(screen.innerHTML !== '0'){
 
       if(screen.innerHTML[0] !== '-'){
         temp = '-' + screen.innerHTML;
       }else{
-        temp = screen.innerHTML.substr(1);
-      }      
-
-      screen.innerHTML = temp
-      data.currentNumber = temp;
-
-    }
-
-  }
-
-  let createNumber = (val) => {
-
-
-    if( data.finished === true ){  
-      data.currentNumber = '';  
-      data.finished = false;
-    }
-
-    if( data.currentNumber.length < 12 ){
-      if( data.isZero === true  ){
-        data.currentNumber = val;   
-        data.isZero = false;             
-      }else{
-        if( screen.innerHTML !== '0' ){
-          data.currentNumber += val;
-        }else{
-          data.currentNumber = val;
-        }        
+       temp = screen.innerHTML.substr(1);
       }
+      screen.innerHTML = temp;
 
     }
-
-    screen.innerHTML = data.currentNumber;
-    checkZero(); 
-    //screen.innerHTML = formatNumber(data.currentNumber);
-
   }
 
-  // Try to implement this function in all the displays, including the subscreen
-  let formatNumber = (num) => {
-
-    let displayNum;
-    let x = num;
-
-    if(data.currentNumber.length > 9){     
-      displayNum = x.slice(0, 3) + "," + x.slice(3, 6) + "," + x.slice(6, 9) + "," + x.slice(9, 14);  
-      return displayNum;   
-    }else if(data.currentNumber.length > 6){   
-      displayNum = x.slice(0, 3) + "," + x.slice(3, 6) + "," + x.slice(6, 9);      
-      return displayNum; 
-    }else if(data.currentNumber.length > 3){  
-      displayNum = x.slice(0, 3) + "," + x.slice(3, 6); 
-      return displayNum;        
-    }else{
-      return x;
-    }
-
+  let clearLast = () => {
+    screen.innerHTML = '0';
   }
 
-  let getNum = (num) => {
-    let numArr = data[num];
-    if(numArr === ''){ numArr = '0'};
-    return numArr;
-  }
-
-
-  let operation = (val) => {
-
-    console.log('isZero');
-    console.log(data.isZero);
-    console.log('currentNumber');
-    console.log(data.currentNumber);
-    console.log('previousNumber');
-    console.log(data.previousNumber);
-    console.log('result');
-    console.log(data.result);
-    console.log('substring');
-    console.log(data.substring);
-    console.log('-----------------------');
-
-    if( data.isZero === false ){
-
-      data.substring.push(getNum('currentNumber'), val);   
-      data.previousNumber = data.currentNumber;
-      data.currentNumber = '';
-      subscreen.innerHTML = data.substring.join(' ');   
-      doMath(val);     
-
-    }else{
-
-      if(val === '='){
-        //data.currentNumber === data.previousNumber;
-        doMath(val);
-      }else{
-        data.substring[data.substring.length-1] = val;
-        subscreen.innerHTML = data.substring.join(' '); 
-      }     
-
-    }
-
-    console.log('substring after');
-    console.log(data.substring);
-    console.log('-----------------------');
-
-    
-  
-  }
-
-  let doMath = (val) => {
-
-    let currentOperation = data.substring[data.substring.length-3];
-
-    if(!currentOperation){
-      currentOperation = '+';
-    }
-    
-    let numA = data.result;
-    let numB = Number(getNum('previousNumber'));
-    let myResult;
-
-    //console.log('numA');
-    //console.log(numA);
-    //console.log('currentOperation');
-    //console.log(currentOperation);
-    //console.log('numB');
-    //console.log(numB);
-
-    if(currentOperation === '/'){
-
-      myResult = numA / numB;
-
-    }else if(currentOperation === 'x'){
-
-      myResult = numA * numB;
-
-    }else if(currentOperation === '+'){
-
-      myResult = numA + numB;
-
-    }else if(currentOperation === '-'){
-
-      myResult = numA - numB;
-
-    }
-
-    data.result = round(myResult, 10);
-    
-    //console.log('result');
-    //console.log(data.result);
-
-    screen.innerHTML = data.result;
-
-    //console.log('-----------------------');
-
-    if(val === '='){
-      subscreen.innerHTML = '';
-      data.currentNumber = data.result; 
-      data.previousNumber = '';
-      data.substring = [];
-      data.result = 0;
-      data.finished = true;
-
-      //console.log('previousNumber');
-      //console.log(data.previousNumber);
-      //console.log('result');
-      //console.log(data.result);
-      //console.log('substring');
-      //console.log(data.substring);
-    }
-
-    checkZero();
-
-    console.log('******************');
-    console.log('isZero');
-    console.log(data.isZero);
-    console.log('******************');
-
-  }
-
-  let resetAll = () => { // for C
-
-    data.currentNumber = '';
-    data.previousNumber = '';
-    data.substring = [];
-    data.result = 0;
+  let clear = () => {
+    preMath = 0;
+    result = 0;
+    operation = '';
+    chain = false;
+    firstNoneZero = false;
     screen.innerHTML = '0';
     subscreen.innerHTML = '';
-
   }
 
-  let resetLast = (num) => {
-    screen.innerHTML = '0';
-    data.currentNumber = '';
-  }
-
+  //round function
   function round(number, precision) {
     var shift = function (number, precision) {
       var numArray = ("" + number).split("e");
@@ -262,17 +263,6 @@ window.onload = function() {
     return shift(Math.round(shift(number, +precision)), -precision);
   }
 
-
 }
 
-
-
-/*
-TO DO:
-- negative empty number returns NaN
--operations jus when there is a number
-- dont display zero as the first number (s)
-- Add comas to the display
-- Check all the functionality
-*/
 
